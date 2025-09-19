28f41d57808f570fbfd81c6a0f1c8cbb067be7c7
lebid15 <alayatl.tr@gmail.com>
2025-09-19 14:06:47 +0300

    fix(backend): set STATIC_ROOT and add Pillow for ImageField; unbreak collectstatic and builds

--- backend/mujard/settings.py
+++ backend/mujard/settings.py
@@ -144,7 +144,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# Serve static files under /static/ and collect them into BASE_DIR/static (mounted in production)
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
